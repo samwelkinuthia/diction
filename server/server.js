5439afd597c3239bda19837f0958e7583aac9491
samwelkinuthia <samwuelkinuthia@gmail.com>
2022-01-17 08:40:14 +0300

    applied regex to filter out special characters from text string

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const axios = require('axios');
 const request = require('request');
 const unfluff = require('unfluff');
 const url = "https://en.wikipedia.org/wiki/Star_Wars";
+const express = require('express');
+const cors = require('cors');
 
-request(url, function (error, response, body) {
+request(url, function(error, response, body) {
   data = unfluff(body)
-  console.log(data.text);
+  const text = data.text.replace(/[^a-zA-Z ]/g, "")
+  // console.log(sorted);
 });
