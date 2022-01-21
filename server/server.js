48d5a7fdd1c8af582307259e8dab14719add2a06
samwelkinuthia <samwuelkinuthia@gmail.com>
2022-01-21 20:24:49 +0300

    created endpoint that processes url to text conversion. Response sent to client is an object containing words as the keys and the occurence as the respective values

--- server/server.js
+++ server/server.js
@@ -6,56 +6,48 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const routes = express.Router();
+// const routes = express.Router();
 const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// get driver connection
-//
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
-//
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
 
 app.post("/count", (req, res) => {
-  res.send('Obtained')
+    let url = req.body.url;
+    request(url, function(error, response, body) {
+        let data = unfluff(body)
+        const text = data.text.replace(/[^a-zA-Z ]/g, "")
+        const sorted = wordFreq(text)
+        const arranged = sorter(sorted)
+        for (const key in arranged) {
+            if (key === '') {
+                delete arranged[key]
+            }
+            // if (arranged[key] > 2) {
+            //     console.log(`${key} : ${arranged[key]}`);
+            // }
+        }
+        res.send(arranged);
+    });
 })
 
 // const myObj = { you: 100, me: 75, foo: 116, bar: 15 };
 
 // console.log((JSON.stringify(result)));
 
-request(url, function(error, response, body) {
-  data = unfluff(body)
-  const text = data.text.replace(/[^a-zA-Z ]/g, "")
-  const sorted = wordFreq(text)
-  const arranged = sorter(sorted)
-
-  for (const key in arranged) {
-    if (key === '') {
-      delete arranged[key]
-    }
-    if (arranged[key] > 20) {
-      console.log(`${key} : ${arranged[key]}`);
-
-    }
-  }
-  // console.log(sorted);
-  const objectArr = Object.entries(sorted).map((e) => ({
-    [e[0]]: e[1]
-  }));
-});
 
 function sorter(obj) {
-  const result = Object
-   .entries(obj)
-   .sort((a, b) => b[1] - a[1])
-   .reduce((_sortedObj, [k,v]) => ({
-     ..._sortedObj,
-     [k]: v
-   }), {});
-   return result
+    return Object
+       .entries(obj)
+       .sort((a, b) => b[1] - a[1])
+       .reduce((_sortedObj, [k, v]) => ({
+           ..._sortedObj,
+           [k]: v
+       }), {})
 }
 
 function wordFreq(string) {
