ce9b7f831f24cb2534a1cc4ed67aff94c60ba7f1
samwelkinuthia <samwuelkinuthia@gmail.com>
2022-01-22 22:21:12 +0300

    removed unused axios package, regenerate package.lock to fix undefined property errors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const request = require('request');
 const unfluff = require('unfluff');
 const url = "https://en.wikipedia.org/wiki/Star_Wars";
@@ -18,18 +17,21 @@ app.listen(port, () => {
 
 app.post("/count", (req, res) => {
     let url = req.body.url;
+    // let url2 = req.body.url2
     request(url, function(error, response, body) {
         let data = unfluff(body)
         const text = data.text.replace(/[^a-zA-Z ]/g, "")
+        const wordArray = text.split(" ")
+        const length = wordArray.length;
         const sorted = wordFreq(text)
         const arranged = sorter(sorted)
         for (const key in arranged) {
             if (key === '') {
                 delete arranged[key]
             }
-            // if (arranged[key] > 2) {
-            //     console.log(`${key} : ${arranged[key]}`);
-            // }
+            if (arranged[key] < 2) {
+                delete arranged[key]
+            }
         }
         const objArr = [];
         Object.keys(arranged).forEach(key => objArr.push({
@@ -38,20 +40,43 @@ app.post("/count", (req, res) => {
             }
         ));
         console.log(objArr)
+        // console.log(res.send())
         // console.log(objArr);
-        res.send(objArr);
+        res.send({objs:objArr, dist: length});
     });
 })
 
 
-// console.log((JSON.stringify(result)));
+app.post("/count2", (req, res) => {
+    let url1 = req.body.url1;
+    let url2 = req.body.url2;
+    // let url2 = req.body.url2
+    request(url, function(error, response, body) {
+        let data = unfluff(body)
+        const text = data.text.replace(/[^a-zA-Z ]/g, "")
+        const wordArray = text.split(" ")
+        const length = wordArray.length;
+        const sorted = wordFreq(text)
+        const arranged = sorter(sorted)
+        for (const key in arranged) {
+            if (key === '') {
+                delete arranged[key]
+            }
+            if (arranged[key] < 2) {
+                delete arranged[key]
+            }
+        }
+        const objArr = [];
+        Object.keys(arranged).forEach(key => objArr.push({
+                word: key,
+                count: arranged[key]
+            }
+        ));
+        console.log(objArr)
+        res.send({objs:objArr, dist: length});
+    });
+})
 
-function convertCollection(obj) {
-    let keys = Object.keys(obj);
-    return keys.map(function(key) {
-        return ({ id: key, ...obj[key] })
-    })
-}
 function sorter(obj) {
     return Object
        .entries(obj)
